37bf79d49e256f837501301afd6c0cd6fa0c4575
https://github.com/Sadd1 <sheikhsaddi1679@gmail.com>
2021-02-25 21:52:37 +0600

    HOME PAGE

---
+++ MID_LAB_TASK_PHP_7/HOME.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+     
+     
+     <title>Home Page</title>
+
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr> <td>
+                    <img src='logo.png'   height='100px'/>
+                </td> 
+                <td align='right'>
+                    <nav><a href='./home.php'>Home</a> |<a href='./login.php'>Login</a> |<a href='./reg.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            <h1><b> Welcome to X Company</b></h1>  <br><br><br><br>
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+			<b>
+                Copyright © 2017
+                
+				</b>
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>
