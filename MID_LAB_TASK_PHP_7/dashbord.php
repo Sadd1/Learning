d4f5be103e68647848c4e7101dca1b6a655a3536
https://github.com/Sadd1 <sheikhsaddi1679@gmail.com>
2021-02-25 22:53:37 +0600

    dashbord

---
+++ MID_LAB_TASK_PHP_7/dashbord.php
@@ -0,0 +1,33 @@
+ <html>
+<head>
+ <title> Dashboard form</title>
+</head>
+<body>
+ <fieldset>
+ <table width='100%' >
+<tr><td> <img src='logo.png'  height='60px'/></td> <td align='right'>
+
+  <nav> 
+  <label> You are Logged in as a User <a href='./home.php'> </a> </label>|
+  <a href='./logout.php'>Log Out</a>
+
+</nav></td></tr>
+</table>
+</fieldset>
+ <table>
+<tr><td><label>Account</label><br><hr>
+
+       <ul> <li><a href='#'>Dashboard</a></li><li><a href='#'> View Profile</a></li><li>
+       <a href='#'> Edit Profile</a></li><li><a href='#'> Change Profile Picture</a></li><li>
+       <a href='#'>Change Password</a></li> <li><a href='#'>Logout</a></li> </ul></td></tr></td>
+</table>
+<fieldset>
+ <center>
+
+   <label>  Copyright © 2017  </label>
+
+</center>
+
+</fieldset>
+</body>
+</html>
