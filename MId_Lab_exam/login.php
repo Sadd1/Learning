0063a15d551384d08eeee7b484edf304d3bc1ba3
https://github.com/Sadd1 <sheikhsaddi1679@gmail.com>
2021-03-14 13:09:57 +0600

    2nd commit

---
+++ MId_Lab_exam/login.php
@@ -0,0 +1,56 @@
+
+
+
+
+
+<html>
+<head>
+     
+    <title>Registration form</title>
+</head>
+<body>
+    <fieldset>
+        <legend>
+            <b>Login</b>
+        </legend>
+        <form action='#' method='POST'>
+            <table> 
+                <tr>
+                    <td>
+                        User ID:
+                    </td>
+                    <td>
+                        <input type="User_ID" name='User_id'>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Password:
+                    </td>
+                    <td>
+                        <input type="password" name='password'>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <hr>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td colspan="2">
+                        <input type='submit' value='Submit'>
+                        <a href="url">Register</a>
+                        
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </fieldset>
+</body>
+</html>
